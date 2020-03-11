3fb0bdb6fa568023b51e41b7f94312dee2c90cca
gdpettus <gabe.pettus@gmail.com>
2020-03-11 02:51:53 -0600

    minor cleanup

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 mongoose.connect(MONGODB_URI);
 
 // routes
